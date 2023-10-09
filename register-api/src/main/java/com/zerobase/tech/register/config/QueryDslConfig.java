a5a25cd962a3cb9f35374ff53180df4417a9b615
kwonjoonhyuk <jkatwxc1@naver.com>
2023-10-09 15:12:29 +0900

    querydsl config 파일

--- register-api/src/main/java/com/zerobase/tech/register/config/QueryDslConfig.java
+++ register-api/src/main/java/com/zerobase/tech/register/config/QueryDslConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+//Querydsl Bean 등록
 @Configuration
 public class QueryDslConfig {
 
